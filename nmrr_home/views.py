e312846abcb89f1cf7f96695fa1b7420193fa45f
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2018-04-23 11:34:05 -0400

    refactor: NMRR theme / Homepage
    Change-Id: I7ceec632098a7979bb6752b7a4bb98caaf228187

--- nmrr_home/views.py
+++ nmrr_home/views.py
@@ -15,34 +15,43 @@ def tiles(request):
         "tiles": []
     }
 
-    if "core_curate_app" in installed_apps:
-        curate_tile = {
-            "logo": "fa-edit",
-            "link": reverse("core_curate_index"),
-            "title": "Curate your Materials Data",
-            "text": "Click here to select a form template and then fill out the corresponding form."
-        }
-
-        context["tiles"].append(curate_tile)
-
-    if "core_explore_example_app" in installed_apps:
-        explore_example_tile = {
-            "logo": "fa-flask",
-            "link":  reverse("core_explore_example_index"),
-            "title": "Build your own queries",
-            "text": "Click here to search for Materials Data in the repository using flexible queries."
-        }
-
-        context["tiles"].append(explore_example_tile)
-
     if "core_explore_keyword_app" in installed_apps:
         explore_keywords_tile = {
             "logo": "fa-search",
             "link":  reverse("core_explore_keyword_app_search"),
-            "title": "Search by keyword",
-            "text": "Click here to explore for Materials Data in the repository using keywords."
+            "title": "Search for resources",
+            "text": "Click here to explore the Registry using keywords."
         }
 
         context["tiles"].append(explore_keywords_tile)
+
+    if "core_curate_app" in installed_apps:
+        curate_tile = {
+            "logo": "fa-edit",
+            "link": reverse("core_curate_index"),
+            "title": "Add your resource",
+            "text": "Click here to add a new Resource to the Registry."
+        }
+
+        context["tiles"].append(curate_tile)
+
+    if not request.user.is_authenticated():
+        login_tile = {
+            "logo": "fa-sign-in",
+            "link": reverse("core_main_app_login"),
+            "title": "Login",
+            "text": "Click here to login to the Registry."
+        }
+
+        context["tiles"].append(login_tile)
+    else:
+        logout_tile = {
+            "logo": "fa-sign-out",
+            "link": reverse("core_main_app_logout"),
+            "title": "Logout",
+            "text": "Click here to logout from the Registry."
+        }
+
+        context["tiles"].append(logout_tile)
 
     return render(request, "nmrr_home/tiles.html", context)
