a341328f31fc940bf78ffa5233af0c66574f447c
Adrien Catel <adrien.catel@nist.gov>
2018-10-10 11:20:53 -0400

    refactor: nist header/footer context processor
    Change-Id: Ie2fe3dc6561947cf84b1f2900eccc9533d1278d1

--- nmrr/settings.py
+++ nmrr/settings.py
@@ -108,7 +108,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 "core_main_app.utils.custom_context_processors.domain_context_processor",  # Needed by any curator app
-                "nmrr.custom_context_processors.domain_context_processor",  # Needed by NMRR
                 "django.template.context_processors.i18n",
             ],
         },
