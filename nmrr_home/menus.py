a7fe2b5727b8dc2b0f05b5afdfd6e9c1f12869ac
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-06-15 16:16:03 -0400

    feat: add help menu
    Change-Id: I7293017fe834cb991eeaf2f901f28e73ff848895

---
+++ nmrr_home/menus.py
@@ -0,0 +1,23 @@
+""" Menu configuration for nmrr.
+
+  * Help menu
+
+    * API Documentation
+    * Contact
+    * Help
+"""
+
+from django.urls import reverse
+from menu import Menu, MenuItem
+
+Menu.add_item(
+    "help", MenuItem("API Documentation", reverse("swagger_view"), icon="cogs")
+)
+
+Menu.add_item(
+    "help", MenuItem("Contact", reverse("core_website_app_contact"), icon="envelope")
+)
+
+Menu.add_item(
+    "help", MenuItem("Help", reverse("core_website_app_help"), icon="question-circle")
+)
