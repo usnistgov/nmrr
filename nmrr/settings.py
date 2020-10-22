a316611d1c2341e5fc7e487dacc17c94c6cf0d69
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-10-22 09:43:21 -0400

    fix: update nmrr settings for PID
    Change-Id: I6aa6d06a68834c3336b4f04ef23bf6ddd8e3520c

--- nmrr/settings.py
+++ nmrr/settings.py
@@ -503,8 +503,16 @@ ID_PROVIDER_SYSTEMS = {
         "class": "core_linked_records_app.utils.providers.local.LocalIdProvider",
         "args": [],
     },
+    # "handle": {
+    #     "class": "core_linked_records_app.utils.providers.handle_net.HandleNetSystem",
+    #     "args": [
+    #         "https://handle-server.example.org:8000",
+    #         "300%3APREFIX/USER",
+    #         "password",
+    #     ],
+    # },
 }
-""" dict: provider systems available for registring PIDs
+""" dict: provider systems available for registring PIDs.
 """
 
 ID_PROVIDER_PREFIXES = ["cdcs"]
