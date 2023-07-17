6a3fe7732cc28b7bfcd94135fb1af63d9ddddcaf
Guillaume Sousa <guillaume.sousa@nist.gov>
2023-07-17 15:57:09 -0400

    fix: test settings
    Change-Id: Ifccaacdcd657c65fbfbe9e9339f0d1ace20b0555

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,3 +1,6 @@
+""" Settings for nmrr tests
+"""
+
 SECRET_KEY = "fake-key"
 
 INSTALLED_APPS = [
@@ -5,10 +8,51 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
     "django.contrib.sites",
     # Local apps
     "core_main_app",
-    "core_main_app.tests",
 ]
+
+# In-memory test DB
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+
+MIDDLEWARE = (
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+)
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
+            ],
+        },
+    },
+]
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CELERYBEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
 MONGODB_INDEXING = False
 MONGODB_ASYNC_SAVE = False
+USE_TZ = True
